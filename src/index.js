07b6090b74f4504b190d183081989c41e6d3fa03
JaeyeongSon <nardi0302@gmail.com>
2022-08-16 03:23:26 +0900

    FEAT: 최적화 조 짜기 임시 기능 완성
    조 짜기 버튼만으로 이용할 수 있도록 핵심 기능만 구현하고,
    필요한 데이터는 하드코딩 하였습니다.
    
    Todo
    
    - 데이터 하드코딩 하지 말고 input으로 받아오기
    - 보관이 필요한 데이터 localStorage 이용하여 브라우저에 저장하기

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-const worker = new Worker('./src/worker.js');
+import tabMenu from './tabMenu.js';
+import Table from './Table.js';
+
+const worker = new Worker('./src/worker/index.js');
+
+// 테스트를 위한 임시 코드
+
+const peopleArr = [];
+for (let i = 0; i < 17; i++) {
+	peopleArr.push(i);
+}
 
 const data = {
 	rounds: [],
@@ -8,11 +18,52 @@ const data = {
 	forbiddenPairs: [],
 };
 
+const zero = [
+	'강지승',
+	'김경현',
+	'김민석',
+	'김은우',
+	'김이주',
+	'김채린',
+	'김현진',
+	'박윤하',
+	'박지윤',
+	'백남헌',
+	'손재영',
+	'신민경',
+	'유은지',
+	'이채련',
+	'전희준',
+	'조한',
+	'최현정',
+];
+
 function submitData(e) {
 	worker.postMessage(data);
 }
 
+const $form = document.querySelector('#tabpanel-1 form');
+const $testBtn = document.querySelector('.makeGroup__submit button');
+const $resultBox = document.querySelector('.result__content');
+
+const resultTable = new Table({
+	$target: $resultBox,
+	initialState: { members: zero, groups: data.rounds[0] },
+});
+
+$form.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
+
+$testBtn.addEventListener('click', (e) => {
+	submitData();
+});
+
 worker.addEventListener('message', (e) => {
 	console.log(e.data);
 	data.rounds = [...e.data.rounds];
+	resultTable.setState({
+		members: zero,
+		groups: data.rounds[data.rounds.length - 1],
+	});
 });
