86ba7977f74688317ec1cb50821ab17f18c51a31
chaerin-dev <chaerin.dev@gmail.com>
2022-08-25 23:52:20 +0900

    feat: create left side bar
    왼쪽 사이드바 추가

--- src/App.js
+++ src/App.js
@@ -1,1 +1,37 @@
-export default function App($target) {}
+export default class App {
+  constructor({ $target }) {
+    this.state = {
+      selectedTab: "",
+    };
+
+    this.$sidebar = document.createElement("div");
+    this.$sidebar.className = "sidebar";
+    $target.appendChild(this.$sidebar);
+
+    this.$contentsWrapper = document.createElement("div");
+    this.$contentsWrapper.className = "contentsWrapper";
+    $target.appendChild(this.$contentsWrapper);
+
+    this.renderSidebar = (() => {
+      this.$sidebar.innerHTML = `
+    <span class="logo">GROUP-MAKER</span>
+    <ul class="sidebar__tabBtnList">
+      <li><button>NewGroup</button></li>
+      <li><button>Members</button></li>
+      <li><button>Records</button></li>
+    </ul>
+    <ul class="sidebar__infoBtnList">
+      <li><button>GITHUB</button></li>
+      <li><button>ISSUE</button></li>
+      <li><button>뭐시기</button></li>
+      <li><button>저시기</button></li>
+    </ul>
+    `;
+    })();
+
+    this.$tabBtnList = document.querySelector(".sidebar__tabBtnList");
+    this.$tabBtnList.addEventListener("click", (e) => {
+      this.$contentsWrapper.innerHTML = e.target.innerHTML;
+    });
+  }
+}
