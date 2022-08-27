aedc037b1adcb3eba5df7bba6918dcf49daab3ba
chaerin-dev <chaerin.dev@gmail.com>
2022-08-28 02:55:29 +0900

    feat: create modal

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,13 +1,16 @@
+import Modal from '../Common/Modal.js';
+
 export default class Members {
-  constructor($target, membersData) {
+  constructor($target, membersData, modal) {
     this.init = () => {
+      // 서브타이틀이랑 멤버 리스트 담을 공간 만들어!
       $target.innerHTML = `
       <h2>MANAGE MEMBERS</h2>
       <ul class="membersList"></ul>
       `;
 
+      // 멤버리스트 공간을 멤버 이름, 수정버튼, 삭제버튼으로 채워줘!
       this.$membersList = document.querySelector('.membersList');
-
       for (const memberKey of Object.keys(membersData)) {
         this.$membersList.innerHTML += `
         <li>
@@ -17,30 +20,22 @@ export default class Members {
         </li>`;
       }
 
+      // 마지막에 멤버 추가 버튼 추가해줘!
       this.$membersList.innerHTML += `
       <li>
         <button class="addBtn">Add Member</button>
-        <div>
-          <form class="addForm displayNone">
-            <input class="addName" type="text"></input>
-            <button class="addConfirmBtn" type="submit">Add!</button>
-          </form>
-        </div>
       </li>
       `;
 
+      // 모달 추가해줘!
+      this.$modal = new Modal($target);
+      this.$membersList += this.$modal;
+      this.$modalWrapper = document.querySelector('.modalWrapper');
+
+      // 멤버 추가 버튼 관련 이벤트 처리
       this.$addBtn = document.querySelector('.addBtn');
-      this.$addForm = document.querySelector('.addForm');
-      this.$addName = document.querySelector('.addName');
-      this.$addConfirmBtn = document.querySelector('.addConfirmBtn');
       this.$addBtn.addEventListener('click', e => {
-        this.$addBtn.classList.add('displayNone');
-        this.$addForm.classList.remove('displayNone');
-      });
-      this.$addConfirmBtn.addEventListener('click', e => {
-        e.preventDefault();
-        this.$addBtn.classList.remove('displayNone');
-        this.$addForm.classList.add('displayNone');
+        this.$modalWrapper.classList.remove('displayNone');
       });
     };
   }
