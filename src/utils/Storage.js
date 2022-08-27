dd1d221ff07bf82bac18dcfc2f00ee6336c55f57
JaeyeongSon <nardi0302@gmail.com>
2022-08-27 21:08:45 +0900

    Revert "FEAT: Create Storage component"
    This reverts commit 6c32411de5d129ae1aec8271b3b54226208b0152.

--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -1,34 +1,27 @@
-export class Storage {
-  constructor({ storageId, defaultValue = {} }) {
-    this.storage = localStorage;
-    this.storageId = storageId;
-    this.defaultValue = defaultValue;
-  }
+export default function Storage({ storageId, defaultValue = [] }) {
+	this.storage = localStorage;
 
-  getItem() {
-    try {
-      const data = this.storage.getItem(this.storageId);
-      return data ? JSON.parse(data) : this.defaultValue;
-    } catch (err) {
-      throw new Error('localStorage parse Error in getItem', err);
-    }
-  }
+	this.getItem = () => {
+		try {
+			const data = this.storage.getItem(storageId);
+			return data ? JSON.parse(data) : defaultValue;
+		} catch (err) {
+			throw new Error('localStorage parse Error in getItem', err);
+		}
+	};
 
-  setItem(value) {
-    try {
-      this.storage.setItem(this.storageId, JSON.stringify(value));
-    } catch (err) {
-      throw new Error('localStorage setItem Error', err);
-    }
-  }
+	this.setItem = (value) => {
+		try {
+			this.storage.setItem(storageId, JSON.stringify(data));
+		} catch (err) {
+			throw new Error('localStorage setItem Error', err);
+		}
+	};
 
-  removeItem(key) {
-    try {
-      this.storage.removeItem(key);
-    } catch (err) {
-      throw new Error('localStorage remove Error', err);
-    }
-  }
+	this.removeItem = (key) => {
+		try {
+		} catch (err) {
+			throw new Error('localStorage remove Error', err);
+		}
+	};
 }
-
-export default Storage;
