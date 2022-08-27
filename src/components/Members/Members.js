b99808657b1607b6c350015642caebcdb6f673ff
chaerin-dev <chaerin.dev@gmail.com>
2022-08-27 23:09:52 +0900

    feat: create add member, edit member, delete memeber button

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -6,13 +6,42 @@ export default class Members {
       <ul class="membersList"></ul>
       `;
 
-      const $membersList = document.querySelector('.membersList');
+      this.$membersList = document.querySelector('.membersList');
 
       for (const memberKey of Object.keys(membersData)) {
-        $membersList.innerHTML += `<li>${membersData[memberKey].name}</li>`;
+        this.$membersList.innerHTML += `
+        <li>
+          ${membersData[memberKey].name}
+          <button>EDIT</button>
+          <button>DELETE</button>
+        </li>`;
       }
 
-      $membersList.innerHTML += `<li><button>Add Member</button></li>`;
+      this.$membersList.innerHTML += `
+      <li>
+        <button class="addBtn">Add Member</button>
+        <div>
+          <form class="addForm displayNone">
+            <input class="addName" type="text"></input>
+            <button class="addConfirmBtn" type="submit">Add!</button>
+          </form>
+        </div>
+      </li>
+      `;
+
+      this.$addBtn = document.querySelector('.addBtn');
+      this.$addForm = document.querySelector('.addForm');
+      this.$addName = document.querySelector('.addName');
+      this.$addConfirmBtn = document.querySelector('.addConfirmBtn');
+      this.$addBtn.addEventListener('click', e => {
+        this.$addBtn.classList.add('displayNone');
+        this.$addForm.classList.remove('displayNone');
+      });
+      this.$addConfirmBtn.addEventListener('click', e => {
+        e.preventDefault();
+        this.$addBtn.classList.remove('displayNone');
+        this.$addForm.classList.add('displayNone');
+      });
     };
   }
 }
